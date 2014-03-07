b9837399f45c24f8a0a2faae90e5d1ec6622cf8e
iraasta <iraasta@gmail.com>
2014-03-07 02:50:16 +0100

    performance optimization

--- app.js
+++ app.js
@@ -4,20 +4,70 @@
 
 
 //========IMPORTS============
-var net = require("net");
 var framecut = require("./framecut");
 //==========END==============
 
-framecut.initByDelimiter(handleData, "|");
-framecut.debug = true;
+var times = 100000;
+var dataCB = 0;
+var dataCS = 0;
+var time = process.uptime();
+for(var i = 0 ; i < 5 ; i++)
+    handleByte();
 
-net.createServer(function(socket){
-    socket.on("data", function(data){
-        framecut.handleFrame(data,socket);
-    })
-} ).listen(1908);
-
-function handleData(content, client)
+function handleByte()
 {
-    console.log(content);
+    var cb = function()
+    {
+        counter--;
+        if(counter <= 0)
+        {
+            time = (process.uptime() - time)*1000;
+            console.log("Byte: "+ time + "ms");
+            console.log((dataCB/1024/128)/(time/1000) + "MBit/s would be a limit\n");
+            handleString();
+        }
+    };
+    var time = process.uptime();
+    framecut.initByLength(cb, 1);
+    framecut.debug = false;
+    var i = times;
+    var counter = i;
+    var someDataB = new Buffer([7,0,1,2,3,4]);
+    var someDataB2 = new Buffer([5,6,7]);
+    while(i-- > 0)
+    {
+        framecut.handleFrame(someDataB, null);
+        framecut.handleFrame(someDataB2, null);
+
+        dataCB+= 8;
+    }
+}
+function handleString()
+{
+    var cb = function()
+    {
+        counter--;
+
+        if(counter <= 0)
+        {
+            time = (process.uptime() - time)*1000;
+            console.log("String: "+ time + "ms");
+            console.log((dataCS/1024/128)/(time/1000) + "Mbit/s would be a limit\n");
+
+        }
+    };
+    //String
+    var time = process.uptime();
+    framecut.initByDelimiter(cb, "|");
+    framecut.debug = false;
+    var i = times;
+    var counter = i;
+    var someDataS = new Buffer("1234");
+    var someDataS2 = new Buffer("56|");
+    while(i-- > 0)
+    {
+        framecut.handleFrame(someDataS, null);
+        framecut.handleFrame(someDataS2, null);
+        dataCS+=8;
+    }
 }
--- framecut.js
+++ framecut.js
@@ -36,6 +36,10 @@ framecut.initByLength = function(cb , frameLengthSize){
     _byLength = true;
     _frameLL = frameLengthSize || _frameLL;
     _cb = cb;
+    framecut.handleFrame = function(data, client)
+    {
+        return handleFrameL(data,_frame,client);
+    }
 } ;
 /**
  *
@@ -46,6 +50,10 @@ framecut.initByDelimiter = function(cb , delimiter){
     _byLength = false;
     _delimiter = delimiter || delimiter;
     _cb = cb;
+    framecut.handleFrame = function(data, client)
+    {
+        return handleFrameD(data ,client);
+    }
 } ;
 
 /**
@@ -54,34 +62,25 @@ framecut.initByDelimiter = function(cb , delimiter){
  * @param client
  * @returns {Buffer|String}
  */
-framecut.handleFrame = function(data, client)
-{
-    if(_byLength) return handleFrameL(data,_frame,client);
-    else          return handleFrameD(data ,client);
-};
+framecut.handleFrame = function(data, client){return null};
+
+
 
 function handleFrameL(data, frame, client)
 {
     _frame = Buffer.concat([frame,data]);
-    console.log(_frame.toJSON());
     if(_frame.length  >= _frameLL && !_currentFrameLength)
     {
         _currentFrameLength = Buffer.concat([new Buffer(new Array(4-_frameLL)),
             _frame.slice(0,_frameLL)],
             4).readInt32BE(0);
-        log("Got The Frame Length " + _currentFrameLength);
     }
     if(_currentFrameLength != 0)
     {
-        log("Got The Frame Part " + data.toJSON());
         if(_frame.length >= _currentFrameLength + _frameLL)
         {
             var content = _frame.slice(_frameLL,_currentFrameLength + _frameLL);
-            log("Got The Whole Frame " + _frame.toJSON());
-            log("Got The Whole Content " + content.toJSON());
-
             _cb(content, client);
-
 
             //Reset the data after finishing
             if(content.length+1 < _currentFrameLength + _frameLL)
@@ -96,22 +95,18 @@ function handleFrameL(data, frame, client)
     }
     return _frame;
 }
-
+var cake;
+var i;
+var last;
 function handleFrameD(data, client)
 {
     _word += data.toString();
-    //console.log(data);
-   // console.log(_word);
-    var cake = _word.split(_delimiter);
-    var i = cake.length - 1;
+    cake = _word.split(_delimiter);
+    last = cake.length - 1;
+    i = last;
     while(--i > -1)
     {
         _cb(cake[i], client);
     }
-    _word = cake[cake.length-1];
-}
-
-function log(str)
-{
-    if(framecut.debug) console.log(str);
+    _word = cake[last];
 }
